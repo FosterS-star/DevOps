a635523f0f95cff5c93cd93eaa7d5f70c7171666
FosterS-star <salipurevic.stefan@outlook.com>
2020-01-25 15:52:34 +0100

    first commit

---
+++ DevOps/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            Console.WriteLine("Enter first number: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            int secondNumber;
+            Console.WriteLine("Enter second number: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int result = firstNumber + secondNumber;
+            Console.WriteLine("Result is: " + result);
+            Thread.Sleep(1000);
+        }
+    }
+}
